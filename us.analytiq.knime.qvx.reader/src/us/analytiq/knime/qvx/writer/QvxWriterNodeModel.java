f393950400b783c579c5967e5dcd6c34dacf5777
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2019-05-29 19:07:13 +0200

    Fix error message on writer node execution

--- us.analytiq.knime.qvx.reader/src/us/analytiq/knime/qvx/writer/QvxWriterNodeModel.java
+++ us.analytiq.knime.qvx.reader/src/us/analytiq/knime/qvx/writer/QvxWriterNodeModel.java
@@ -76,7 +76,7 @@ public class QvxWriterNodeModel extends NodeModel {
     	if (mSettings.getFileName().equals("")) {
             throw new InvalidSettingsException("No settings available");
         }
-        return new DataTableSpec[]{null};
+        return null;
     }
 
     @Override
